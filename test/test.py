6d65217077e51576c98e24021feddd600fca76e4
HVT06 <harshvardhantiwari106@hotmail.com>
2024-05-08 04:21:24 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -17,8 +17,8 @@ async def test_project(dut):
     # Reset
     dut._log.info("Reset")
     dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
+    dut.data_in1.value = 0
+    dut.data_in2.value = 0
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
@@ -34,7 +34,7 @@ async def test_project(dut):
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    assert dut.data_out1.value == 50
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.

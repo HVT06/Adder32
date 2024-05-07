2a99103db97c61dfe5de992b9f249143be1195b2
HVT06 <harshvardhantiwari106@hotmail.com>
2024-05-08 04:09:13 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -15,16 +15,22 @@ module tb ();
 
   // Wire up the inputs and outputs:
   reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
+  reg clear;
+  // reg ena;
+  reg [7:0] data_in1;
+  reg [7:0] data_in2;
+  reg [7:0] data_in3;
+  reg [7:0] data_in4;
+  reg [7:0] data_in5;
+   
+  wire [7:0] data_out1;
+  wire [7:0] data_out2;
+  wire [7:0] data_out3;
+  wire [7:0] data_out4;
+  wire [7:0] data_out5;
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_systolic_array user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
@@ -32,14 +38,18 @@ module tb ();
       .VGND(1'b0),
 `endif
 
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
+      .data_in1  (data_in1),    // Dedicated inputs
+     .data_in2  (data_in2),    // Dedicated inputs
+     .data_in3  (data_in3),    // Dedicated inputs
+     .data_in4  (data_in4),    // Dedicated inputs
+     .data_in5  (data_in5),    // Dedicated inputs
+      .data_out1 (data_out1),   // Dedicated outputs
+     .data_out2 (data_out2),   // Dedicated outputs
+     .data_out3 (data_out3),   // Dedicated outputs
+     .data_out4 (data_out4),   // Dedicated outputs
+     .data_out5 (data_out5),   // Dedicated outputs
+     .clear    (clear),      // enable - goes high when design is selected
       .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
   );
 
 endmodule
